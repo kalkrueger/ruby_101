fe774822528f42a0a9dd763d7f184a5dd5702210
Kal Krueger <kkrueger19@gmail.com>
2020-10-28 12:36:41 -0600

    tic tac update

--- lesson_6/tic_tac_toe.rb
+++ lesson_6/tic_tac_toe.rb
@@ -1,7 +1,7 @@
-require 'pry'
 INITIAL_MARKER = " "
 PLAYER_MARKER = "X"
 COMPUTER_MARKER = "O"
+PLAYER_1 = 'choose' # choose, PLAYER_MARKER, or COMPUTER_MARKER
 
 WINNING_COMBOS = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] +
                  [[1, 4, 7], [2, 5, 8], [3, 6, 9]] +
@@ -29,6 +29,63 @@ def display_board(brd)
   puts "     |     |  "
 end
 # rubocop:enable Metrics/AbcSize
+
+def choose
+  answer = ''
+  loop do
+    prompt "Who do you want to go first? Player or Computer?"
+    answer = gets.chomp.downcase
+    if answer.start_with?('p')
+      answer = PLAYER_MARKER
+      break
+    elsif answer.start_with?('c')
+      answer = COMPUTER_MARKER
+      break
+    else
+      prompt "Please choose a vaild option."
+    end
+  end
+  answer
+end
+
+def whos_first
+  turn = ''
+  if PLAYER_1 == COMPUTER_MARKER
+    turn = COMPUTER_MARKER
+  elsif PLAYER_1 == PLAYER_MARKER
+    turn = PLAYER_MARKER
+  else
+    turn = choose
+  end
+  turn
+end
+
+def whos_up(counter, first_up)
+  turn = ''
+  if counter.odd? && first_up == PLAYER_MARKER
+    turn = COMPUTER_MARKER
+  elsif counter.odd? && first_up == COMPUTER_MARKER
+    turn = PLAYER_MARKER
+  else
+    turn = first_up
+  end
+  turn
+end
+
+def play_piece(turn, brd)
+  turn = turn
+  loop do
+    display_board(brd)
+    break if someone_won?(brd) || board_full?(brd)
+    if turn == PLAYER_MARKER
+      turn = COMPUTER_MARKER
+      player_places_piece!(brd)
+    elsif turn == COMPUTER_MARKER
+      turn = PLAYER_MARKER
+      computer_places_piece!(brd)
+    end
+  end
+end
 
 def initialize_board
   new_board = {}
@@ -60,12 +117,18 @@ def player_places_piece!(brd)
 end
 
 def computer_places_piece!(brd)
-  square = ''
-  if defend(brd)[0]
-    square = defend(brd)[0][0]
-  else
-    square = valid_sqaures(brd).sample
+  square = nil
+  WINNING_COMBOS.each do |line|
+    square = attack(line, brd)
+    break if square
   end
+  if !square
+    WINNING_COMBOS.each do |line|
+      square = defend(line, brd)
+      break if square
+    end
+  end
+  square = valid_sqaures(brd).sample if !square
   brd[square] = COMPUTER_MARKER
 end
 
@@ -88,13 +151,17 @@ def detect_winner(brd)
   nil
 end
 
-def defend(brd)
-  player_choices = brd.select{ |k, v| v == PLAYER_MARKER}.keys
-  REMAINING_COMBOS.each do |arr|
-    arr.delete_if { |n| player_choices.include?(n)}
+def defend(line, brd)
+  if brd.values_at(*line).count(PLAYER_MARKER) == 2
+    brd.select { |k, v| line.include?(k) && v == INITIAL_MARKER }.keys.first
   end
-  REMAINING_COMBOS.select do
-    |arr| arr.size == 1 && valid_sqaures(brd).include?(arr[0])
+end
+
+def attack(line, brd)
+  if valid_sqaures(brd).include?(5)
+    5
+  elsif brd.values_at(*line).count(COMPUTER_MARKER) == 2
+    brd.select { |k, v| line.include?(k) && v == INITIAL_MARKER }.keys.first
   end
 end
 
@@ -106,28 +173,22 @@ end
 loop do
   player_score = 0
   computer_score = 0
+  counter = 0
+  first_up = whos_first
   loop do
     board = initialize_board
 
-    REMAINING_COMBOS = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] +
-                       [[1, 4, 7], [2, 5, 8], [3, 6, 9]] +
-                       [[1, 5, 9], [3, 5, 7]]
+    turn = whos_up(counter, first_up)
 
-    loop do
-      display_board(board)
-      player_places_piece!(board)
-      break if someone_won?(board) || board_full?(board)
-      computer_places_piece!(board)
-      break if someone_won?(board) || board_full?(board)
-    end
-
-    display_board(board)
+    play_piece(turn, board)
 
     if someone_won?(board)
-      prompt "#{detect_winner(board)} won the match!"
+      prompt "#{detect_winner(board)} won the game!"
     else
       prompt "It's a tie!"
     end
+
+    counter += 1
 
     case detect_winner(board)
     when "Player"
@@ -138,12 +199,11 @@ loop do
 
     prompt "Player: #{player_score}, Computer: #{computer_score}"
     if player_score == 5 || computer_score == 5
-      prompt "#{detect_winner(board)} won the game!"
+      prompt "#{detect_winner(board)} won the series!"
       break
     else
       continue
     end
-
   end
 
   prompt "Would you like to play again? (y/n)"
